0bb06fec593ef6c6344f1b84672d0cda5826d88f
QuinSDev <quindev97@gmail.com>
2023-11-20 18:06:34 -0500

    Se crea la lógica para que un usuario pueda hacer una reserva en un quincho específico, guardando la fecha de entrada y la de la salidad lo mismo que el checkIn y el checkOut, además se le da algunos efectos de hover a las CardQuincho

--- src/components/CardQuincho.jsx
+++ src/components/CardQuincho.jsx
@@ -36,32 +36,32 @@ export const CardQuincho = ({ quinchos }) => {
 
         <div className="mt-6 grid grid-cols-1 gap-x-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {quinchos.map((quincho) => (
-            <div key={quincho.id} className="relative rounded-md shadow-2xl">
+            <div key={quincho.id} className="relative rounded-md shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] hover:shadow-gray-400 hover:scale-105 duration-700">
               {/*imagen*/}
-              <div className="w-full overflow-hidden rounded-md bg-gray-200 lg:h-80 group-hover:opacity-75">
+              <div className="mt-2 w-full overflow-hidden  bg-gray-200 lg:h-52 group-hover:opacity-75">
                 {/*hover:opacity-75*/}
                 <img
                   src={quincho.photoUrl}
                   alt="imagen del quincho"
                   loading="lazy" // Agrega el atributo loading="lazy"
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  className="w-full object-cover object-center lg:h-full lg:w-full"
                 />
-                <span aria-hidden="true" >
+                <span aria-hidden="true">
                   <img className="h-8 w-8" src={heart} alt="Logo de corazon" />
                 </span>
               </div>
 
-              <div className="m-4">
+              <div className="m-5 mt-3">
                 {/*Titulo*/}
-                <div className="mt-4 flex justify-between">
+                <div className="flex justify-between">
                   <div>
                     <span>{showIcon(quincho.typeQuincho)}</span>
                   </div>
 
                   <div>
-                    <h3 className="mt-1 text-l font-bold text-gray-900">
+                    <h3 className="mt-0.5 text-lg font-bold text-gray-900">
                       <a>
-                        <span aria-hidden="true"  />
+                        <span aria-hidden="true" />
                         {quincho.nameQuincho}
                       </a>
                     </h3>
@@ -69,7 +69,7 @@ export const CardQuincho = ({ quinchos }) => {
                 </div>
 
                 {/*Direccion*/}
-                <div className="mt-4 flex justify-between">
+                <div className="mt-2.5 flex justify-between">
                   <div>
                     <span>
                       <img
@@ -81,10 +81,9 @@ export const CardQuincho = ({ quinchos }) => {
                   </div>
 
                   <div>
-                    <h3 className="mt-1 text-sm text-gray-500">
+                    <h3 className="mt-0.5 text-sm text-gray-500">
                       <a>
-                        <span aria-hidden="true" 
-                        />
+                        <span aria-hidden="true" />
                         {quincho.location}
                       </a>
                     </h3>
@@ -92,7 +91,7 @@ export const CardQuincho = ({ quinchos }) => {
                 </div>
 
                 {/*Precio*/}
-                <div className="mt-4 flex justify-between">
+                <div className="mt-2.5 flex justify-between">
                   <div>
                     <span>
                       <img
@@ -104,23 +103,21 @@ export const CardQuincho = ({ quinchos }) => {
                   </div>
 
                   <div>
-                    <h3 className="mt-1 font-semibold text-sm text-gray-500">
+                    <h3 className="mt-0.5 font-semibold text-sm text-gray-500">
                       <a>
-                        <span aria-hidden="true" />
-                        $ {quincho.price} Por noche
+                        <span aria-hidden="true" />$ {quincho.price} Por noche
                       </a>
                     </h3>
                   </div>
                 </div>
-
-                <button className="mt-8 btnQuincho cursor-pointer">
-                  <Link
-                    to="/quinchosDetails"
-                    onClick={() => handleDetailsSelectedQuincho(quincho)}
-                  >
+                <Link
+                  to="/quinchosDetails"
+                  onClick={() => handleDetailsSelectedQuincho(quincho)}
+                >
+                  <button className="mt-8 btnQuincho cursor-pointer">
                     Ver detalles
-                  </Link>
-                </button>
+                  </button>
+                </Link>
               </div>
             </div>
           ))}
